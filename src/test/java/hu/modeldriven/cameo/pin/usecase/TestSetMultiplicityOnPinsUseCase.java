1cc1d959b4fccc42dde5e60dddd52a916658e15b
Zsolt Sandor <zsolt.sandor@gmail.com>
2022-11-01 22:09:17 +0100

    Improved tests

--- src/test/java/hu/modeldriven/cameo/pin/usecase/TestSetMultiplicityOnPinsUseCase.java
+++ src/test/java/hu/modeldriven/cameo/pin/usecase/TestSetMultiplicityOnPinsUseCase.java
@@ -1,18 +1,20 @@
 package hu.modeldriven.cameo.pin.usecase;
 
 import com.nomagic.uml2.ext.magicdraw.actions.mdbasicactions.Pin;
-import hu.modeldriven.cameo.pin.MagicDrawMock;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.LiteralInteger;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.LiteralUnlimitedNatural;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.ValueSpecification;
 import hu.modeldriven.cameo.pin.event.ApplyChangeRequestedEvent;
 import hu.modeldriven.cameo.pin.event.CloseDialogRequestedEvent;
 import hu.modeldriven.cameo.pin.event.PinMultiplicitySetEvent;
 import hu.modeldriven.cameo.pin.model.ModelElementId;
-import hu.modeldriven.cameo.pin.model.multiplicity.DefaultMultiplicityModels;
 import hu.modeldriven.cameo.pin.model.multiplicity.OneToOneMultiplicity;
 import hu.modeldriven.cameo.pin.ui.PinPanel;
 import hu.modeldriven.core.eventbus.EventBus;
 import hu.modeldriven.core.magicdraw.MagicDraw;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -21,6 +23,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -39,38 +42,48 @@ class TestSetMultiplicityOnPinsUseCase {
     @Mock
     PinPanel pinPanel;
 
-    @InjectMocks
-    DefaultMultiplicityModels defaultMultiplicityModels;
-
-    @InjectMocks
-    MagicDrawMock magicDrawMock;
-
     @Test
-    void testNoActiveProjectGeneratesCloseDialogRequestedEvent(){
+    void testNoActiveProjectGeneratesCloseDialogRequestedEvent() {
         when(magicDraw.existsActiveProject()).thenReturn(false);
         eventBus.publish(new ApplyChangeRequestedEvent());
         verify(eventBus).publish(any(CloseDialogRequestedEvent.class));
     }
 
     @Test
-    void testOneToOneRuleAppliedToSinglePin(){
+    void testOneToOneRuleAppliedToSinglePin() {
+
+        // given we have a selected pin and the multiplicity is set to 1..1
 
         var pin = mock(Pin.class);
-        var modelElementId = new ModelElementId(UUID.randomUUID().toString());
 
-        when(magicDraw.getElementByID(any())).thenReturn(pin);
-        when(pinPanel.getSelectedMultiplicity()).thenReturn(new OneToOneMultiplicity(magicDraw));
-        when(pinPanel.getModelElementIds()).thenReturn(Sets.newSet(modelElementId));
+        var literalInteger = mock(LiteralInteger.class);
+        var literalUnlimitedNatural = mock(LiteralUnlimitedNatural.class);
+
         when(magicDraw.existsActiveProject()).thenReturn(true);
+        when(magicDraw.getElementByID(any())).thenReturn(pin);
+        when(magicDraw.createLiteralInteger(anyInt())).thenReturn(literalInteger);
+        when(magicDraw.createLiteralUnlimitedNatural(anyInt())).thenReturn(literalUnlimitedNatural);
+
+        when(pinPanel.getSelectedMultiplicity()).thenReturn(new OneToOneMultiplicity(magicDraw));
+        when(pinPanel.getModelElementIds()).thenReturn(Sets.newSet(new ModelElementId(UUID.randomUUID().toString())));
+
+        // when the user press apply
 
         eventBus.publish(new ApplyChangeRequestedEvent());
 
-        verify(eventBus).publish(any(PinMultiplicitySetEvent.class));
-        verify(pin).setLowerValue(any());
-        verify(pin).setUpperValue(any());
+        // then an event is created
+        // indicating that the pin's lower and upper value was set to a corresponding value
 
-        // the correct lower and upper value setter is called for the pin
-        }
+        verify(eventBus).publish(any(PinMultiplicitySetEvent.class));
+
+        var lowerValueCaptor = ArgumentCaptor.forClass(ValueSpecification.class);
+        verify(pin).setLowerValue(lowerValueCaptor.capture());
+        assertEquals(literalInteger, lowerValueCaptor.getValue());
+
+        var upperValueCaptor = ArgumentCaptor.forClass(ValueSpecification.class);
+        verify(pin).setUpperValue(upperValueCaptor.capture());
+        assertEquals(literalUnlimitedNatural, upperValueCaptor.getValue());
+    }
 
 
 }
