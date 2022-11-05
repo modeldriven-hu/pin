62edaa89ac09a76317bb852a09b88e729d224434
Zsolt Sandor <zsolt.sandor@gmail.com>
2022-11-05 16:05:41 +0100

    Added test for usecase

--- src/test/java/hu/modeldriven/cameo/pin/usecase/TestCloneNameAndTypeFromPinUseCase.java
+++ src/test/java/hu/modeldriven/cameo/pin/usecase/TestCloneNameAndTypeFromPinUseCase.java
@@ -3,13 +3,16 @@ package hu.modeldriven.cameo.pin.usecase;
 import com.nomagic.uml2.ext.magicdraw.actions.mdbasicactions.Pin;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.LiteralInteger;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.LiteralUnlimitedNatural;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Type;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.ValueSpecification;
 import hu.modeldriven.cameo.pin.event.ApplyChangeRequestedEvent;
 import hu.modeldriven.cameo.pin.event.CloseDialogRequestedEvent;
 import hu.modeldriven.cameo.pin.event.PinMultiplicitySetEvent;
+import hu.modeldriven.cameo.pin.model.CloneMethod;
 import hu.modeldriven.cameo.pin.model.CloneSource;
 import hu.modeldriven.cameo.pin.model.ModelElementId;
 import hu.modeldriven.cameo.pin.model.Multiplicity;
+import hu.modeldriven.cameo.pin.model.clone.CloneSourceImpl;
 import hu.modeldriven.cameo.pin.model.multiplicity.*;
 import hu.modeldriven.core.eventbus.EventBus;
 import hu.modeldriven.core.magicdraw.MagicDraw;
@@ -20,6 +23,9 @@ import org.mockito.*;
 import org.mockito.internal.util.collections.Sets;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -44,6 +50,125 @@ class TestCloneNameAndTypeFromPinUseCase {
         when(magicDraw.existsActiveProject()).thenReturn(false);
         eventBus.publish(new ApplyChangeRequestedEvent(null, null, null));
         verify(eventBus).publish(any(CloseDialogRequestedEvent.class));
-
     }
+
+    @Test
+    void testCloneName(){
+
+        // given there is an active project and
+
+        when(magicDraw.existsActiveProject()).thenReturn(true);
+
+        // there are multiple pins with id, name and type
+
+        var pin1 = Mockito.mock(Pin.class);
+        var pin2 = Mockito.mock(Pin.class);
+
+        when(pin1.getID()).thenReturn("1");
+        when(pin2.getID()).thenReturn("2");
+
+        when(magicDraw.getElementByID("1")).thenReturn(pin1);
+        when(magicDraw.getElementByID("2")).thenReturn(pin2);
+
+        var modelElementId1 = new ModelElementId("1");
+        var modelElementId2 = new ModelElementId("2");
+
+        when(pin1.getName()).thenReturn("pin1");
+
+        // when one element is selected as the source of clone with an action clone name
+
+        eventBus.publish(new ApplyChangeRequestedEvent(Set.of(modelElementId1, modelElementId2)
+                , null,
+                new CloneSourceImpl(modelElementId1, CloneMethod.NAME)));
+
+        // then the other pins name will be change to the source pin's name
+        verify(pin2).setName("pin1");
+
+        // but the type won't change
+        verify(pin2, never()).setType(any());
+
+        // and no modification occurs on the source pin
+        verify(pin1, never()).setName(anyString());
+        verify(pin1, never()).setType(any());
+    }
+
+    @Test
+    void testCloneType(){
+        // given there is an active project and
+
+        when(magicDraw.existsActiveProject()).thenReturn(true);
+
+        // there are multiple pins with id, name and type
+
+        var pin1 = Mockito.mock(Pin.class);
+        var pin2 = Mockito.mock(Pin.class);
+
+        when(pin1.getID()).thenReturn("1");
+        when(pin2.getID()).thenReturn("2");
+
+        when(magicDraw.getElementByID("1")).thenReturn(pin1);
+        when(magicDraw.getElementByID("2")).thenReturn(pin2);
+
+        var modelElementId1 = new ModelElementId("1");
+        var modelElementId2 = new ModelElementId("2");
+
+        var type = Mockito.mock(Type.class);
+
+        when(pin1.getType()).thenReturn(type);
+
+        // when one element is selected as the source of clone with an action clone name
+
+        eventBus.publish(new ApplyChangeRequestedEvent(Set.of(modelElementId1, modelElementId2)
+                , null,
+                new CloneSourceImpl(modelElementId1, CloneMethod.TYPE)));
+
+        // then the other pins type will be change to the source pin's type
+        verify(pin2).setType(type);
+
+        // but the name won't change
+        verify(pin2, never()).setName(anyString());
+
+        // and no modification occurs on the source pin
+        verify(pin1, never()).setName(anyString());
+        verify(pin1, never()).setType(any());
+    }
+
+    @Test
+    void testCloneNameAndType(){
+        when(magicDraw.existsActiveProject()).thenReturn(true);
+
+        // there are multiple pins with id, name and type
+
+        var pin1 = Mockito.mock(Pin.class);
+        var pin2 = Mockito.mock(Pin.class);
+
+        when(pin1.getID()).thenReturn("1");
+        when(pin2.getID()).thenReturn("2");
+
+        when(magicDraw.getElementByID("1")).thenReturn(pin1);
+        when(magicDraw.getElementByID("2")).thenReturn(pin2);
+
+        var modelElementId1 = new ModelElementId("1");
+        var modelElementId2 = new ModelElementId("2");
+
+        var type = Mockito.mock(Type.class);
+
+        when(pin1.getType()).thenReturn(type);
+        when(pin1.getName()).thenReturn("Pin1");
+
+        // when one element is selected as the source of clone with an action clone name
+
+        eventBus.publish(new ApplyChangeRequestedEvent(Set.of(modelElementId1, modelElementId2)
+                , null,
+                new CloneSourceImpl(modelElementId1, CloneMethod.NAME_AND_TYPE)));
+
+        // then the other pins name and type will be change to the source pin's name and type
+        verify(pin2).setName("Pin1");
+        verify(pin2).setType(type);
+
+        // but no modification occurs on the source pin
+        verify(pin1, never()).setName(anyString());
+        verify(pin1, never()).setType(any());
+    }
+
 }
